e58a36c825d46352ee255577c2023cb39a91f44b
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-09-19 13:10:47 -0400

    Dumped Euler's method.

--- project1/code/q2.m
+++ project1/code/q2.m
@@ -31,55 +31,6 @@ qp = reshape(y(end, (N+1)*(M+1)+1:end), [N+1, M+1]);
 
 fprintf('t=%f s to t=%f s\n', t(1), t(end));
 
-% Euler's method
-% for n=1:nstep
-%     % Imposing boundary conditions.    
-%     for j=1:M+1
-%         pp(1,j) = 0; % p=p_0 (p'=0) at x=0
-%         pp(N+1,j) = pp(N,j); % dp'/dx = 0 at x=L
-%         
-%         qp(1,j) = 0; % q'=0 at x=0
-%         qp(N+1,j) = qp(N,j); % dq'/dx 0 at x=L
-%     end
-%     
-%     for i=1:N+1
-%         pp(i,M+1) = pp(i,M); % dp'/dy = 0 at y=H
-%         
-%         fprintf('i = %d\n', i);
-%         fprintf('x = %d\n', i*dx);
-%         if i*dx < L/4 || i*dx > L/2
-%             pp(i,1) = pp(i,2); % dp'/dy = 0 at y=0 for x < L/4 and x > L/2.
-%         else
-%             % dp'/dx = (rho_0*u_0^2*Fxx + dp'/dy)/(dF/dx) at y=0 for L/4 < x < L/2.
-%             Fx = (F(i+1) - F(i)) / (dx);
-%             Fxx = (F(i+1) - 2*F(i) + F(i-1))/(dx^2);
-%             ppy = (pp(i,2) - pp(i,1)) / dy;
-%             pp(i+1,1) = pp(i-1,1) + (rho_0*u_0^2*Fxx + ppy)/(2*dx*Fx);
-%         end
-%     end
-%     
-%     % Calculate time derivatives for p' and q' at every grid point.
-%     % We will not worry about calculating derivatives at the boundaries.
-%     for i=2:N
-%         for j=2:M
-%             dppdt(i,j) = qp(i,j) - u_0 * (pp(i+1,j) - pp(i-1,j))/(2*dx);
-%             dqpdt(i,j) = (c_0^2) * ((pp(i+1,j) - 2*pp(i,j) + pp(i-1,j)) / (dx^2)) ...
-%                        + (c_0^2) * ((pp(i,j+1) - 2*pp(i,j) + pp(i,j-1)) / (dy^2)) ...
-%                        - u_0 * ((qp(i+1,j) - qp(i-1,j)) / (2*dx));
-%         end
-%     end
-% 
-%     % Calculate new values of p' and q' at every grid point (Euler's method).
-%     % The boundary is taken care of by the boudary conditions.
-%     for i=2:N
-%         for j=2:M
-%             pp(i,j) = pp(i,j) + dppdt(i,j)*dt;
-%             qp(i,j) = qp(i,j) + dqpdt(i,j)*dt;
-%         end
-%     end
-%     
-% end
-
 x = repmat(linspace(0, L, N+1), M+1, 1);
 yy = repmat(linspace(0, H, M+1)', 1, N+1);
 
