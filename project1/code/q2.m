ffe71917a6a2505f6ea0fd9fc8bd4b6fd2d2b53a
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-09-25 07:11:51 -0400

    Done with figure 1.

--- project1/code/q2.m
+++ project1/code/q2.m
@@ -1,16 +1,18 @@
 function [pp, x, y] = q2(dt, nstep, M, F, ppinitial, qpinitial)
 global pp qp
 
-% Defining problem parameters. Note: all constants are unitless, or rather,
-% non-dimensional.
+% Defining problem parameters. Note: all constants are unitless, or
+% rather, non-dimensional.
 rho_0 = 1;
 c_0 = 1;
 u_0 = sqrt(2);
 L = 6;
 H = 2;
 
-N = 4*(size(F,1)-1); % From the fact that F is of size (N/4 + 1, 1).
+% From the fact that F is of size (N/4 + 1, 1).
+N = 4*(size(F,1)-1);
 
+% Determine step sizes.
 dx = L/N;
 dy = H/M;
 
@@ -22,34 +24,10 @@ F = [zeros(N/4, 1); F; zeros(N/2, 1)];
 pp = ppinitial;
 qp = qpinitial;
 
-% Using ode45.
+% Use ode45 to integrate 
 y0 = [reshape(pp, (N+1)*(M+1), 1); reshape(qp, (N+1)*(M+1), 1)];
 [t, y] = ode45(@(t,y) q2odefun(t, y, N, M, dx, dy, F), [0 nstep*dt], y0, odeset('Stats', 'on'));
-% pp = reshape(y(end, 1:(N+1)*(M+1)), N+1, M+1);
-% qp = reshape(y(end, (N+1)*(M+1)+1:end), N+1, M+1);
-
-fprintf('t=%f s to t=%f s\n', t(1), t(end));
 
 x = repmat(linspace(0, L, N+1), M+1, 1);
-y = repmat(linspace(0, H, M+1)', 1, N+1);
-
-% yy = repmat(linspace(0, H, M+1)', 1, N+1);
-% for i = 1:25:size(y,1)
-%     ppp = reshape(y(i, 1:(N+1)*(M+1)), N+1, M+1);
-%     % qpp = reshape(y(i, (N+1)*(M+1)+1:end), N+1, M+1);
-%     % ppp(:,1) = ppp(:,2);
-%     % qpp(:,1) = qpp(:,2);
-%     % surf(x,yy,qpp');
-%     % shading interp;
-%     % view(2);
-%     % subplot(1,2,1);
-%     contourf(x,yy,ppp');
-%     colorbar;
-%     % subplot(1,2,2);
-%     % contourf(x,yy,qpp');
-%     % colorbar;
-%     drawnow;
-% end
-
 y = repmat(linspace(0, H, M+1)', 1, N+1);
 end
--- project1/code/q2odefun.m
+++ project1/code/q2odefun.m
@@ -9,7 +9,7 @@ u_0 = sqrt(2);
 L = 6;
 H = 2;
 
-% Reshape y vector into p',q' matrices.
+% Reshape y vector into p' and q' matrices for more readable code.
 pp = reshape(y(1:(N+1)*(M+1)), N+1, M+1);
 qp = reshape(y((N+1)*(M+1)+1:end), N+1, M+1);
 
@@ -23,9 +23,10 @@ pp(:,M+1) = pp(:,M); % dp'/dy = 0 at y=H
 
 for i=1:N+1
     if i*dx < L/4 || i*dx > L/2
-        pp(i,1) = pp(i,2); % dp'/dy = 0 at y=0 for x < L/4 and x > L/2.
+        % Imposing dp'/dy = 0 at y=0 for x < L/4 and x > L/2.
+        pp(i,1) = pp(i,2);
     else     
-        % dp'/dy = -rho_0*u_0^2*Fxx
+        % Imposing dp'/dy = -rho_0*u_0^2*Fxx at y=0 for L/4 <= x <= L/2.
         Fxx = (F(i+1) - 2*F(i) + F(i-1))/(dx^2);
 
         % Multiply by 2*dy to continue using central difference with
@@ -38,24 +39,14 @@ end
 % Calculate time derivatives for p' and q' at every interior grid point.
 dppdt = zeros(N+1, M+1);
 dqpdt = zeros(N+1, M+1);
-% dppdt(1,:) = qp(1,:) - u_0 * [diff(pp(1,:)) 0]/dx;
 for i=2:N
-    % dppdt(i,1) = qp(i,1) - u_0 * (pp(i+1,1) - pp(i-1,1))/(2*dx);
     for j=2:M
         dppdt(i,j) = qp(i,j) - u_0 * (pp(i+1,j) - pp(i-1,j))/(2*dx);
         dqpdt(i,j) = (c_0^2) * ((pp(i+1,j) - 2*pp(i,j) + pp(i-1,j)) / (dx^2)) ...
-                   + (c_0^2) * ((pp(i,j+1) - 2*pp(i,j) + pp(i,j-1)) / (dy^2)) ...
+                  + (c_0^2) * ((pp(i,j+1) - 2*pp(i,j) + pp(i,j-1)) / (dy^2)) ...
                    - u_0 * ((qp(i+1,j) - qp(i-1,j)) / (2*dx));
     end
 end
-
-% xi = repmat(linspace(0, L, N+1), M+1, 1);
-% yi = repmat(linspace(0, H, M+1)', 1, N+1);
-% surf(xi,yi,pp');
-% shading interp;
-% view(2);
-% contourf(xi,yi,pp');
-% drawnow;
 
 dydt = [reshape(dppdt, (N+1)*(M+1), 1); reshape(dqpdt, (N+1)*(M+1), 1)];
 end
