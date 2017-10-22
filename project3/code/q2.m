0e0f4527034c3878cc096a845f4ee94a792fcc3c
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-10-22 14:24:55 -0400

    Coded in the 2x2 block matrices B_gamma,delta and built up M!

--- project3/code/q2.m
+++ project3/code/q2.m
@@ -39,5 +39,36 @@ dg_ijdy = {@(x,y) (1+x)/4, @(x,y) -(1+x)/4;  % dg_--/dy, dg_-+/dy
         integrand = @(x,y) alpha*dg1(x,y)*dg2(x,y);
         G = gauss_legendre_quadrature_2D(integrand);
     end
-       
+
+    function B = B(gamma, delta)
+        function [i,j] = four2two(mu)
+            if mu == 1
+                i=1; j=1;
+            elseif mu == 2
+                i=1; j=2;
+            elseif mu == 3
+                i=2; j=1;
+            elseif mu == 4
+                i=2; j=2;
+            end
+        end
+        
+        nu = 0.31;
+        beta = (1-nu)/2;
+        [i,j] = four2two(gamma);
+        [k,l] = four2two(delta);
+        B_11 = G(i,j,k,l,'x','x') + beta*G(i,j,k,l,'y','y');
+        B_12 = nu*G(i,j,k,l,'x','y') + beta*G(i,j,k,l,'y','x');
+        B_21 = beta*G(i,j,k,l,'x','y') + nu*G(i,j,k,l,'y','x');
+        B_22 = beta*G(i,j,k,l,'x','x') + G(i,j,k,l,'y','y');
+        B = [B_11, B_12; B_21, B_22];
+    end
+
+M = zeros(8,8);
+for gamma=1:4
+    for delta=1:4
+        M(2*gamma-1:2*gamma, 2*delta-1:2*delta) = B(gamma, delta);
+    end
+end
+
 end
