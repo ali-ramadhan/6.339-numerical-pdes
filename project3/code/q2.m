1881a024ffecf7e489b7e938d7ae35cfabad5b6f
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-10-22 15:30:17 -0400

    Tried to solve for u_x, u_y but I don't understand my code lol

--- project3/code/q2.m
+++ project3/code/q2.m
@@ -10,8 +10,8 @@ function q2()
                    + f(1/sqrt(3), -1/sqrt(3)) + f(1/sqrt(3), 1/sqrt(3));
     end
 
-g_ij = {@(x,y) (1+x)*(1+y)/4, @(x,y) (1+x)*(1-y)/4;  % g_--, g_-+
-        @(x,y) (1-x)*(1+y)/4, @(x,y) (1-x)*(1-y)/4}; % g_+-, g_++
+g_ij = {@(x,y) (1+x).*(1+y)./4, @(x,y) (1+x).*(1-y)./4;  % g_--, g_-+
+        @(x,y) (1-x).*(1+y)./4, @(x,y) (1-x).*(1-y)./4}; % g_+-, g_++
 
 dg_ijdx = {@(x,y) (1+y)/4,  @(x,y) (1-y)/4;   % dg_--/dx, dg_-+/dx
            @(x,y) -(1+y)/4, @(x,y) -(1-y)/4}; % dg_+-/dx, dg_++/dx
@@ -31,14 +31,16 @@ dg_ijdy = {@(x,y) (1+x)/4, @(x,y) -(1+x)/4;  % dg_--/dy, dg_-+/dy
             end 
         end
         
-        E = 1;
-        nu = 0;
+        E = 1.18e11;  % Young's modulus for Ti-6Al-2Nb-1Ta-0.8Ma [N/m^2]
+        nu = 0.31;
         alpha = E/(1-nu^2);
         dg1 = dg(i,j,m);
         dg2 = dg(k,l,n);
         integrand = @(x,y) alpha*dg1(x,y)*dg2(x,y);
         G = gauss_legendre_quadrature_2D(integrand);
     end
+
+four2two = {[1,1], [1,2], [2,1], [2,2]};
 
     function B = B(gamma, delta)
         function [i,j] = four2two(mu)
@@ -70,5 +72,46 @@ for gamma=1:4
         M(2*gamma-1:2*gamma, 2*delta-1:2*delta) = B(gamma, delta);
     end
 end
+
+E = 1.18e11;  % Young's modulus for Ti-6Al-2Nb-1Ta-0.8Ma [N/m^2]
+nu = 0.31;
+alpha = E/(1-nu^2);
+
+b = zeros(8,1);
+% k = 0;
+% for i=1:2
+%     for j =1:2
+%         k = k+1;
+%         g = g_ij{i,j};
+%         integrand_x = @(x,y) (1/alpha)*g(x,y)*Fx;
+%         integrand_y = @(x,y) (1/alpha)*g(x,y)*Fy;
+%         b(k) = gauss_legendre_quadrature_2D(integrand_x);
+%         b(4+k) = gauss_legendre_quadrature_2D(integrand_y);
+%     end
+% end
+
+% heatmap(M)
+
+Fx = 0;
+Fy = -8e4;
+
+g = g_ij{2,1};
+integrand_y = @(x,y) (1/alpha)*g(x,y)*Fy;
+b(6) = gauss_legendre_quadrature_2D(integrand_y);
+a = M \ b;
+
+ux = @(x,y) a(1).*g_ij{1,1}(x,y) + a(3).*g_ij{1,2}(x,y) + a(5).*g_ij{2,1}(x,y) + a(7).*g_ij{2,2}(x,y);
+uy = @(x,y) a(2).*g_ij{1,1}(x,y) + a(4).*g_ij{1,2}(x,y) + a(6).*g_ij{2,1}(x,y) + a(8).*g_ij{2,2}(x,y);
+x = repmat(linspace(-1,1,10),[10,1]);
+y = repmat(linspace(-1,1,10),[10,1]);
+ux_xy = ux(x,y);
+uy_xy = uy(x,y);
+heatmap(ux_xy);
+figure;
+heatmap(uy_xy);
+figure;
+ux_xy(1,:)
+uy_xy(1,:)
+plot(x(end,:), ux_xy(end,:)', x(end,:), uy_xy(end,:)');
 
 end
