9da46476020645e352bce489f6f076e55f42b4d4
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-10-22 14:23:55 -0400

    Coded in G, the rank-6 pseudotensor/symbol thing.

--- project3/code/q2.m
+++ project3/code/q2.m
@@ -19,4 +19,25 @@ dg_ijdx = {@(x,y) (1+y)/4,  @(x,y) (1-y)/4;   % dg_--/dx, dg_-+/dx
 dg_ijdy = {@(x,y) (1+x)/4, @(x,y) -(1+x)/4;  % dg_--/dy, dg_-+/dy
            @(x,y) (1-x)/4, @(x,y) -(1-x)/4}; % dg_+-/dy, dg_++/dy
 
+    function G = G(i,j,k,l,m,n)
+        % Returns a function corresponding to the m derivative of the g_ij
+        % function, that is dg_ij/dx_m, where i,j = 1,2 where {1,2}->{-,+}
+        % and m = x,y.
+        function dg = dg(i,j,m)
+            if m=='x'
+                dg = dg_ijdx{i,j};
+            elseif m=='y'
+                dg = dg_ijdy{i,j};
+            end 
+        end
+        
+        E = 1;
+        nu = 0;
+        alpha = E/(1-nu^2);
+        dg1 = dg(i,j,m);
+        dg2 = dg(k,l,n);
+        integrand = @(x,y) alpha*dg1(x,y)*dg2(x,y);
+        G = gauss_legendre_quadrature_2D(integrand);
+    end
+       
 end
