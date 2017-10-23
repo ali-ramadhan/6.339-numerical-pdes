d302e8115909f2654c9e6a4058c7d1adabf0d5d0
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2017-10-22 23:39:53 -0400

    Still no luck, M matrix still badly conditioned :(

--- project3-finite-element-methods/code/q2.m
+++ project3-finite-element-methods/code/q2.m
@@ -5,6 +5,12 @@ global E nu dg_ijdx dg_ijdy
 E = 1.18e11; % Young's modulus for titanium Ti-6Al-2Nb-1Ta-0.8Mo [N/m^2]
 nu = 0.31;   % Poisson's ratio for Titanium Ti-6Al-2Nb-1Ta-0.8Mo
 
+% Cell array with function handles for the four g_ij basis functions.
+% i,j = 1 corresponds to the - subscript. i,j = 2 to the + subscript.
+% E.g. g_ij{2,1} returns the g_+- function handle.
+g_ij = {@(x,y) (1+x).*(1+y)./4, @(x,y) (1+x).*(1-y)./4;  % g_--, g_-+
+        @(x,y) (1-x).*(1+y)./4, @(x,y) (1-x).*(1-y)./4}; % g_+-, g_++
+
 % Function handles for the x-derivatives of the basis functions g_ij.
 % Note: Returned function handles are still multivariate (x,y) to remain
 % compatible with our 2D Gauss-Legendre quadrature function, which
@@ -16,13 +22,11 @@ dg_ijdx = {@(x,y)  (1+y)/4, @(x,y)  (1-y)/4;  % dg_--/dx, dg_-+/dx
 dg_ijdy = {@(x,y) (1+x)/4, @(x,y) -(1+x)/4;  % dg_--/dy, dg_-+/dy
            @(x,y) (1-x)/4, @(x,y) -(1-x)/4}; % dg_+-/dy, dg_++/dy
 
-% Cell array with function handles for the four g_ij basis functions.
-% i,j = 1 corresponds to the - subscript. i,j = 2 to the + subscript.
-% E.g. g_ij{2,1} returns the g_+- function handle.
-g_ij = {@(x,y) (1+x).*(1+y)./4, @(x,y) (1+x).*(1-y)./4;  % g_--, g_-+
-        @(x,y) (1-x).*(1+y)./4, @(x,y) (1-x).*(1-y)./4}; % g_+-, g_++
-
 M = zeros(8,8);
+% M = [B(1,1), B(1,2), B(1,3), B(1,4);
+%      B(2,1), B(2,2), B(2,3), B(2,4);
+%      B(3,1), B(3,2), B(3,3), B(3,4);
+%      B(4,1), B(4,2), B(4,3), B(4,4)];
 for gamma=1:4
     for delta=1:4
         M(2*gamma-1:2*gamma, 2*delta-1:2*delta) = B(gamma, delta);
@@ -30,35 +34,40 @@ for gamma=1:4
 end
 
 M
+det(M)
+cond(M)
 
-F_delta = -8e4; % [N/m^2]
+F_delta = 8e4; % [N/m^2]
 alpha = E/(1-nu^2);
 
 b = zeros(8,1);
+g_11 = g_ij{1,1};
+b(2) = -(F_delta/alpha) * g_11(1,0);
+g_12 = g_ij{1,2};
+b(4) = -(F_delta/alpha) * g_12(1,0);
 g_21 = g_ij{2,1};
-g_21(1,0)
 b(6) = -(F_delta/alpha) * g_21(1,0);
 g_22 = g_ij{2,2};
-g_22(1,0)
 b(8) = -(F_delta/alpha) * g_22(1,0);
 
 b
 
 a = M \ b
 
-% ux = @(x,y) a(1).*g_ij{1,1}(x,y) + a(3).*g_ij{1,2}(x,y) + a(5).*g_ij{2,1}(x,y) + a(7).*g_ij{2,2}(x,y);
-% uy = @(x,y) a(2).*g_ij{1,1}(x,y) + a(4).*g_ij{1,2}(x,y) + a(6).*g_ij{2,1}(x,y) + a(8).*g_ij{2,2}(x,y);
-% x = repmat(linspace(-1,1,10),[10,1]);
-% y = repmat(linspace(-1,1,10),[10,1]);
-% ux_xy = ux(x,y);
-% uy_xy = uy(x,y);
+ux = @(x,y) a(1).*g_ij{1,1}(x,y) + a(3).*g_ij{1,2}(x,y) + a(5).*g_ij{2,1}(x,y) + a(7).*g_ij{2,2}(x,y);
+uy = @(x,y) a(2).*g_ij{1,1}(x,y) + a(4).*g_ij{1,2}(x,y) + a(6).*g_ij{2,1}(x,y) + a(8).*g_ij{2,2}(x,y);
+x = repmat(linspace(-1,1,10),[10,1]);
+y = repmat(linspace(-1,1,10),[10,1]);
+ux_xy = ux(x,y);
+uy_xy = uy(x,y);
+% surf(x,y',ux_xy);
 % heatmap(ux_xy);
 % figure;
 % heatmap(uy_xy);
 % figure;
 % ux_xy(1,:)
 % uy_xy(1,:)
-% plot(x(end,:), ux_xy(end,:)', x(end,:), uy_xy(end,:)');
+plot(x(end,:), ux_xy(end,:)', x(end,:), uy_xy(end,:)');
 
 end
 
