01f5bbae03fec4eab29d03ec8d56bef4da636643
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-11-01 17:02:09 -0400

    Finally u_x and u_y are continuous for 2(b).

--- project3-finite-element-methods/code/q2.m
+++ project3-finite-element-methods/code/q2.m
@@ -47,8 +47,10 @@ dg_ijdy_gen = {@(x,y,x0,y0,a,b)  (x-(x0-a))/(4*a*b), ...
        
 %% Problem 2(a): single element (unit square) beam under stress
 % Element parameters
-x0 = 0; y0 = 0;
-a = 1; b = 1;
+x0 = 0;
+y0 = 0;
+a = 1;
+b = 1;
 
 % Build M matrix block by block.
 M = zeros(8,8);
@@ -98,10 +100,11 @@ plot(x(end/2,:), ux_xy(end/2,:)', x(end/2,:), uy_xy(end/2,:)');
 legend('u_x(x,0)', 'u_y(x,0)');
 
 %% Problem 2(b): rectangular beam (composed of N elements) under stress
-N = 10;
+N = 4;
 
+% Assemble the block-diagonal matrix.
 M2b = zeros(4*(N+1), 4*(N+1));
-for n=1:N
+for n=2:N
     x0 = 2*n-1;
     y0 = 0;
     a = 1;
@@ -110,30 +113,32 @@ for n=1:N
     M2 = zeros(8,8);
     for gamma=1:4
         for delta=1:4
-            M2(2*gamma-1:2*gamma, 2*delta-1:2*delta) = ...
-                B_gen(gamma, delta, x0, y0, a, b);
+            M2(2*gamma-1:2*gamma, 2*delta-1:2*delta) = B_gen(gamma, delta, x0, y0, a, b);
         end
     end
     
     M2b(4*n-3:4*n+4, 4*n-3:4*n+4) = fliplr(flipud(M2));
 end
 
+% Imposing the u_x = u_y = 0 BC at the left wall.
+M2b(1:4,1:4) = zeros(4,4);
 M2b(5:8,1:4) = zeros(4,4);
 M2b(1:4,5:8) = zeros(4,4);
 
 b2b = zeros(4*N+4,1);
-b2b(end) = -F_delta * g_11(1,0);
-b2b(end-2) = -F_delta * g_12(1,0);
-b2b(end-4) = -F_delta * g_21(1,0);
-b2b(end-6) = -F_delta * g_22(1,0);
+b2b(end) = -F_delta * g_11(2*N,0);
+b2b(end-2) = -F_delta * g_12(2*N,0);
+b2b(end-4) = -F_delta * g_21(2*N,0);
+b2b(end-6) = -F_delta * g_22(2*N,0);
 
-% b2b(end-6) = -F_delta * g_11(1,0);
-% b2b(end-4) = -F_delta * g_12(1,0);
-% b2b(end-2) = -F_delta * g_21(1,0);
-% b2b(end) = -F_delta * g_22(1,0);
+% b2b(end-6) = -F_delta * g_11(2*N,0);
+% b2b(end-4) = -F_delta * g_12(2*N,0);
+% b2b(end-2) = -F_delta * g_21(2*N,0);
+% b2b(end) = -F_delta * g_22(2*N,0);
 
 a2b = zeros(4*N+4,1);
 a2b(5:end) = M2b(5:end, 5:end) \ b2b(5:end);
+% a2b = M2b \ b2b;
 
 ux_N = [];
 uy_N = [];
@@ -159,9 +164,14 @@ for n=1:N
     x = repmat(linspace(2*n,2*n+2,100), [100,1]);
     y = repmat(linspace(-1,1,100), [100,1]);
     
-    ux_N = [ux_N; ux(x,y)];
-    uy_N = [uy_N; uy(x,y)];
+    ux_N = [ux_N; ux(x,y)'];
+    uy_N = [uy_N; uy(x,y)'];
 end
+
+x = linspace(0,2*N,100*N);
+y = linspace(-1,1,100);
+figure; surf(x,y,ux_N'); title('2(b) u_x'); shading interp; colorbar;
+figure; surf(x,y,uy_N'); title('2(b) u_y'); shading interp; colorbar;
 end
 
 %% Gauss-Legendre quadrature integration
@@ -219,21 +229,6 @@ function dg = dg_gen(i,j,m)
 end
 
 %% Implementation of the rank-6 G_ijkl^mn tensor-like symbol.
-function G = G_gen(i,j,k,l,m,n,x0,y0,a,b)
-    global alpha
-    assert(i==1 || i==2, 'Invalid value for i, not 1 (-) or 2 (+)!')
-    assert(j==1 || j==2, 'Invalid value for j, not 1 (-) or 2 (+)!')
-    assert(k==1 || k==2, 'Invalid value for k, not 1 (-) or 2 (+)!')
-    assert(l==1 || l==2, 'Invalid value for l, not 1 (-) or 2 (+)!')
-    assert(m=='x' || m=='y', 'Invalid value for m, not ''x'' or ''y''!')
-    assert(n=='x' || n=='y', 'Invalid value for m, not ''x'' or ''y''!')
-
-    dg1 = dg_gen(i,j,m);
-    dg2 = dg_gen(k,l,n);
-    integrand = @(x,y) alpha*dg1(x,y,x0,y0,a,b)*dg2(x,y,x0,y0,a,b);
-    G = gauss_legendre_quadrature_2D(integrand, x0-a, x0+a, y0-b, y0+b);
-end
-
 function G = G(i,j,k,l,m,n)
     global alpha
     assert(i==1 || i==2, 'Invalid value for i, not 1 (-) or 2 (+)!')
@@ -247,6 +242,21 @@ function G = G(i,j,k,l,m,n)
     dg2 = dg(k,l,n);
     integrand = @(x,y) alpha*dg1(x,y)*dg2(x,y);
     G = gauss_legendre_quadrature_2D(integrand, -1, 1, -1, 1);
+end
+
+function G = G_gen(i,j,k,l,m,n,x0,y0,a,b)
+    global alpha
+    assert(i==1 || i==2, 'Invalid value for i, not 1 (-) or 2 (+)!')
+    assert(j==1 || j==2, 'Invalid value for j, not 1 (-) or 2 (+)!')
+    assert(k==1 || k==2, 'Invalid value for k, not 1 (-) or 2 (+)!')
+    assert(l==1 || l==2, 'Invalid value for l, not 1 (-) or 2 (+)!')
+    assert(m=='x' || m=='y', 'Invalid value for m, not ''x'' or ''y''!')
+    assert(n=='x' || n=='y', 'Invalid value for m, not ''x'' or ''y''!')
+
+    dg1 = dg_gen(i,j,m);
+    dg2 = dg_gen(k,l,n);
+    integrand = @(x,y) alpha*dg1(x,y,x0,y0,a,b)*dg2(x,y,x0,y0,a,b);
+    G = gauss_legendre_quadrature_2D(integrand, x0-a, x0+a, y0-b, y0+b);
 end
 
 %% Generates and returns the 2x2 block matrices B_gamma,delta.
